df7c3adcc17e44f8229c9305889fd64db24a86f2
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-01-26 15:05:20 -0500

    first commit

---
+++ src/components/ConditionCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="card">
+        <h1 class="title">{{ card.title }}</h1>
+        <p>{{ card.description }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    card: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+.title {
+    font-weight: bold;
+}
+.card {
+    width: 250px;
+    height: 336px;
+    border: 2px solid black;
+    border-radius: 10%;
+}
+</style>
