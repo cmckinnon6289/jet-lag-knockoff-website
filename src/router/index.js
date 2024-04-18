8052bac6c260dfeddedba70cc82a1c9a42ae0754
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-04-18 15:09:23 -0400

    importing certain auth files. need to work on it.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TeamView from '../views/TeamView.vue'
+import PermsService from '@/services/PermsService';
+import store from '@/store/index.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +9,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'team',
-      component: TeamView
+      component: TeamView,
+      beforeEnter(to, from, next) {
+        const perms = PermsService.getUserPerms();
+        if(store.getters.isLoggedIn && perms >= 1){
+          console.log(perms);
+          next()
+        } else{
+          console.log(`fail ${perms}`);
+          next('/login')
+        }
+      }
     }
   ]
 })
